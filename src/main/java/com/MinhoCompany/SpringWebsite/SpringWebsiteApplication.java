3851013d556d33aa044576a10952b700e3848f12
ImMino <ImMin5@github.com>
2021-04-15 08:10:46 +0900

    init repo

---
+++ src/main/java/com/MinhoCompany/SpringWebsite/SpringWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.MinhoCompany.SpringWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsiteApplication.class, args);
+	}
+
+}
